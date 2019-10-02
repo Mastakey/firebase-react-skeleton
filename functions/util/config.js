852aff9bda09b3d03a039e43075d163c23a9e021
Kioh Han <kioh.han@gmail.com>
2019-10-01 21:57:11 -0400

    Backend Setup 1

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDAYRwJLeXTx-cdWyrcJqe6V6sofsVT9ts",
+    authDomain: "skeleton-8fa08.firebaseapp.com",
+    databaseURL: "https://skeleton-8fa08.firebaseio.com",
+    projectId: "skeleton-8fa08",
+    storageBucket: "",
+    messagingSenderId: "30391912830",
+    appId: "1:30391912830:web:f0159b00244f68fcd6cb2f"
+};
